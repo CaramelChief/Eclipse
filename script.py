cb64a83b29b9e37cda918c2d83faf7fc3bce1d20
Caramel <caramelcheif@gmail.com>
2021-05-19 18:40:15 -0400

    try again

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='~')
+bot = commands.Bot(command_prefix='.')
 
 @bot.command()
 async def test(ctx, arg):
     await ctx.send('hello')
+
+@bot.command()
+async def hi(ctx):
+    await ctx.send('Hello ' + str(ctx.author.name) + '! :)')
