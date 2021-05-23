a961775ce6bb3f4d9a671d7ace8e302f4d1686c9
Caramel <caramelcheif@gmail.com>
2021-05-23 11:39:24 -0400

    dotenv stuff

--- script.py
+++ script.py
@@ -1,11 +1,17 @@
 import discord
+import os
+from dotenv import load_dotenv
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='.')
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+bot = commands.Bot(command_prefix='-')
 
 @bot.command()
 async def test(ctx, arg):
-    await ctx.send('hello')
+    await ctx.send(arg)
 
 @bot.command()
 async def hi(ctx):
